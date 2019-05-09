68423f2433e5db47648e0b3c67b08e061201e587
Ernestas Baltinas <ebaltinas@gmail.com>
2019-05-09 22:16:59 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () =>
+        navigator.serviceWorker.register('loaderSW.js')
+        .then(registration => console.log('Service Worker registered'))
+        .catch(err => 'SW registration failed'));
+}
+
+
+document.querySelector(".loading-spinner-container").addEventListener("click", toggleClass);
+
+function toggleClass() {
+    var element = document.getElementsByClassName("loading-spinner")[0];
+    element.classList.toggle("load-complete");
+}
