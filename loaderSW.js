186420ba67b65d3822ae89f03e88e05796628896
ErnestasBaltinas <ebaltinas@gmail.com>
2019-05-09 23:27:45 +0300

    Update loaderSW.js

--- loaderSW.js
+++ loaderSW.js
@@ -19,7 +19,7 @@ self.addEventListener('fetch', event => {
     const request = event.request;
     const url = new URL(request.url);
     if (url.origin === location.origin) {
-      event.respondWith(cacheFirst(request));
+      event.respondWith(networkFirst(request));
     }
   });
   
@@ -27,4 +27,16 @@ self.addEventListener('fetch', event => {
     const cachedResponse = await caches.match(request);
     return cachedResponse || fetch(request);
   }
+
+async function networkFirst(request) {
+  const dynamicCache = await caches.open('news-dynamic');
+  try {
+    const networkResponse = await fetch(request);
+    dynamicCache.put(request, networkResponse.clone());
+    return networkResponse;
+  } catch (err) {
+    const cachedResponse = await dynamicCache.match(request);
+    return cachedResponse || await caches.match('./fallback.json');
+  }
+}
   
